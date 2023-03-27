5aeb7920eb6690185858342bf5f2261fd5968c47
NatSama2 <vet.nat.c@gmail.com>
2023-03-27 19:16:45 -0300

    funcion sortData

--- main.js
+++ main.js
@@ -4,8 +4,36 @@ console.log(data.dogs[0]);
 
 const dogInfo = document.getElementById('Mostrar perritos');
 
+//mostrar data
 data.dogs.forEach((dog) => { 
     const perrito = `<h2>${dog.breed}</h2>
                      <img src="${dog.image}" class="dog-image">`;
     dogInfo.innerHTML += perrito;
 });
+
+// Funcion ordenar
+function ordenarPerritos(dogs) {
+    dogInfo.innerHTML = "";
+    dogs.forEach((dog) => {
+      const perrito = `<h2>${dog.breed}</h2>
+                       <img src="${dog.image}" class="dog-image">`;
+      dogInfo.innerHTML += perrito;
+    });
+  }
+  
+  // Mostrar datos
+  ordenarPerritos(data.dogs);
+  
+  // Funcion ordenar datos
+  const orderSelect = document.getElementById("order-select");
+  orderSelect.addEventListener("change", function () {
+    const selectedOption = orderSelect.value;
+  
+    const sortedData = data.dogs.sort((a, b) => {
+      return selectedOption === "asc"
+        ? a.breed.localeCompare(b.breed)
+        : b.breed.localeCompare(a.breed);
+    });
+  
+    ordenarPerritos(sortedData);
+  });
